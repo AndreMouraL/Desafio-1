f99355db17e6723eb18e253c711ac21351ba6e7b
André Moura <andremoura0995@gmail.com>
2025-01-31 00:36:38 -0300

    Desafio 1

---
+++ Desafio 1/Problema 1/prob01.js
@@ -0,0 +1,4 @@
+//1- Crie uma variável chamada nome e atribua seu nome a ela. Em seguida, exiba o valor dessa variável.
+console.log("Desafio 1 trilhas 2B")
+var nome = "Andre Moura"; // nome nome = "Andre Moura"; (Global).
+console.log(nome);
