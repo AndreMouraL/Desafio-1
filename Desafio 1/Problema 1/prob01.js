5812cb8f398cd638ab095181cfc32d1a8dcabc6b
André Moura <andremoura0995@gmail.com>
2025-01-31 03:14:07 -0300

    Desafio 1

--- Desafio 1/Problema 1/prob01.js
+++ Desafio 1/Problema 1/prob01.js
@@ -1,6 +1,8 @@
 //1- Crie uma variável chamada nome e atribua seu nome a ela. Em seguida, exiba o valor dessa variável.
 
 //Solução para alert para navegador podemos declarar da seguinte forma
-
-let nome = "Andre Moura"; // let declarar variáveis em JavaScript.
+alert('Boas vindas ao nosso site!');
+let nome = 'Andre Moura'; // let declarar variáveis em JavaScript.
 alert(nome) //exibe uma caixa de diálogo no navegador com o valor da variável nome
+
+//@AndreMouraL
---
+++ Desafio 1/Problema 3/prob03.js
@@ -0,0 +1,20 @@
+//3- Crie uma variável chamada “preco” com o valor 50 e uma variável desconto com o valor 0.2 (20%). Calcule o preço com desconto e exiba o valor final.
+//Navegador
+alert('Boas vindas ao nosso site!');
+
+let preco = 50; // Preço original
+let desconto = 0.2; // Desconto de 20%
+
+// Calculando o preço com desconto
+let precoComDesconto = preco - (preco * desconto);
+
+alert(`O preço final com desconto é: R$${precoComDesconto}`);
+
+/*// no terminal node.js
+let preco = 50; // Preço original
+let desconto = 0.2; // 20% de desconto
+
+// Exibindo o valor final diretamente
+console.log(`O preço final com desconto é: R$${(preco -(preco * desconto)).toFixed(2)}`);*/
+
+//@AndreMouraL
