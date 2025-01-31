caead76ebec31a5f2202f735f618ce07288cbbd8
André Moura <andremoura0995@gmail.com>
2025-01-31 12:58:08 -0300

    Desafio 1

---
+++ Desafio 1/Problema 9/prob09.js
@@ -0,0 +1,15 @@
+//9- Crie um loop que imprima os números de 1 a 10 na tela.
+//No navegador
+alert('Boas vindas ao nosso site!');
+// Loop para imprimir os números de 1 a 10
+for (let i = 1; i <= 10; i++) {
+    alert(i);
+}
+//No node.js
+/*for (let i = 1; i <= 10; i++) {
+    if (i === 11) {
+        break; // O loop vai parar quando i for 11, mas como i vai até 10, não vai parar antes
+    }
+    console.log(i);
+}*/
+//@AndreMoural
