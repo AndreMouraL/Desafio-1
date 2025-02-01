5b0946b1479869ab898bfe59e6c8de42ec972188
André Moura <andremoura0995@gmail.com>
2025-02-01 13:07:53 -0300

    Desafio 1

---
+++ Desafio 1/Problema 1/prob01.js
@@ -0,0 +1,15 @@
+//1- Crie uma variável chamada nome e atribua seu nome a ela. Em seguida, exiba o valor dessa variável.
+
+//Solução para alert para navegador podemos declarar da seguinte forma
+alert('Boas vindas ao nosso site!');
+let nome = 'Andre Moura'; // let declarar variáveis em JavaScript.
+alert(nome) //exibe uma caixa de diálogo no navegador com o valor da variável nome
+
+
+//@AndreMouraL
+/*//Outra forma usando node.js terminal
+console.log('Desafio 1 trilhas 2B')
+let nome = 'Andre Moura'; // nome nome = "Andre Moura"; (Global).
+console.log(nome);*/
+
+//@AndreMouraL
