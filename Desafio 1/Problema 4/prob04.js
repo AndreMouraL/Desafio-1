8fe5fd031b992e7710e9495ebfe9bfff890e0286
André Moura <andremoura0995@gmail.com>
2025-02-01 13:10:56 -0300

    Desafio 1

---
+++ Desafio 1/Problema 4/prob04.js
@@ -0,0 +1,19 @@
+//4- Crie uma variável chamada temperatura e atribua o valor 30. Se a temperatura for maior que 25, exiba a mensagem "Está calor!". Caso contrário, exiba "Está fresco!".
+alert('Boas vindas ao nosso site!');
+
+let temperatura = 30;
+
+if (temperatura > 25) {
+    alert('Está calor!');
+} else {
+    alert('Está fresco!');
+}
+//node.js
+/*let temperatura = 30; // Atribui o valor 30 à variável temperatura
+
+if (temperatura > 25) {
+    console.log("Está calor!");
+} else {
+    console.log("Está fresco!");
+}*/
+//@AndreMouraL
