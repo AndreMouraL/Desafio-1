d476861ecf50ec02015c6ae5e309e90f26e681fc
André Moura <andremoura0995@gmail.com>
2025-02-01 13:13:26 -0300

    Desafio 1

---
+++ Desafio 1/Problema 8/prob08.js
@@ -0,0 +1,10 @@
+//8- Crie uma variável chamada nome e uma variável chamada idade. Exiba a mensagem "Olá, meu nome é [nome] e eu tenho [idade] anos", utilizando concatenação.
+//No navegador
+alert('Boas vindas ao nosso site!');
+
+let nome = 'Andre Moura';
+let idade = 25;
+
+alert('Olá, meu nome é ' + nome + ' e eu tenho ' + idade + ' anos.');
+
+//@AndreMoural
